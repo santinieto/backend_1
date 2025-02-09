7a5bb5f13203715eb4614e697221d09d4b48ace9
Santiago Enrique Nieto <santi.nieto@live.com>
2025-02-09 13:01:51 -0300

    Agrego la funcionalidad para mostrar la lista de productos en la ruta raiz

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import ProductsManager from "..//products_manager/products_manager.js";
+import path from "path";
+import __dirname from "../utils/dirname.js";
 
 const viewsRouter = express.Router();
+const productsManager = new ProductsManager(path.join(__dirname, "/public/data/products.json"));
 
 // Ruta raiz
-viewsRouter.get('/', (req, res) => {
-    res.render('home');
+viewsRouter.get('/', async (req, res) => {
+    await productsManager.loadProducts();
+    const products = await productsManager.getProducts();
+    
+    res.render('home', { products });
 });
 
 viewsRouter.get('/realtimeproducts', (req, res) => {
